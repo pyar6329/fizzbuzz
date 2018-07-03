511075e36073c983b3696451d9304f0163320b59
Tsukasa Arima <pyar6329@gmail.com>
2018-07-03 23:11:37 +0900

    add elixir fizzbuzz

---
+++ elixir/README.md
@@ -0,0 +1,23 @@
+# FizzBuzz
+
+elixir version: > 1.6
+
+## Installation
+
+```bash
+$ git clone git@github.com:pyar6329/fizzbuzz.git
+$ cd fizzbuzz/elixir
+```
+
+## Usage
+
+```bash
+$ iex -S mix
+mix> FizzBuzz.run()
+```
+
+## Test
+
+```bash
+$ mix test
+```
---
+++ elixir/lib/fizz_buzz.ex
@@ -0,0 +1,37 @@
+defmodule FizzBuzz do
+  def run do
+    1..100
+    |> Enum.to_list()
+    |> Enum.map(&fizzbuzz/1)
+    |> Enum.each(&IO.puts/1)
+  end
+
+  # private関数はdoctestが使えないので、暫定的に普通の関数にする
+  @doc ~S"""
+    iex> FizzBuzz.fizzbuzz(1)
+    "1"
+
+    iex> FizzBuzz.fizzbuzz(2)
+    "2"
+
+    iex> FizzBuzz.fizzbuzz(3)
+    "Fizz"
+
+    iex> FizzBuzz.fizzbuzz(5)
+    "Buzz"
+
+    iex> FizzBuzz.fizzbuzz(6)
+    "Fizz"
+
+    iex> FizzBuzz.fizzbuzz(15)
+    "FizzBuzz"
+  """
+  def fizzbuzz(number) do
+    case {rem(number, 3), rem(number, 5)} do
+      {0, 0} -> "FizzBuzz"
+      {0, _} -> "Fizz"
+      {_, 0} -> "Buzz"
+      {_, _} -> Integer.to_string(number)
+    end
+  end
+end
